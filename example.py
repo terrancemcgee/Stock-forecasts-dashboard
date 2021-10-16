420f8efcb72858c82267596126f8ff28e088673e
hamzarabi3 <googlinghamza@gmail.com>
2021-10-16 19:26:28 +0000

    statefull form input

--- example.py
+++ example.py
@@ -2,20 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title("Uber pickups in NYC")
 
-DATE_COLUMN = 'date/time'
-key='6a3b16cf358e1b21ed1ce04e12c415f7e1184dca' #TODO : Put this in a text file and read it
+DATE_COLUMN = "date/time"
+key = "6a3b16cf358e1b21ed1ce04e12c415f7e1184dca"  # TODO : Put this in a text file and read it
 
 
-interval=''
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+interval = ""
+DATA_URL = (
+    "https://s3-us-west-2.amazonaws.com/"
+    "streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
 
-def load_data(inerval,asset): #TODO: use cache
+
+def load_data(inerval, asset):  # TODO: use cache
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(lowercase, axis="columns", inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
-
--- python_engineer_project.py
+++ python_engineer_project.py
@@ -1,7 +1,7 @@
 from mimetypes import init
 from numpy.core.fromnumeric import std
-import pandas as pd 
-import streamlit as st 
+import pandas as pd
+import streamlit as st
 from prophet import Prophet, forecaster
 import datetime
 from prophet.plot import plot, plot_components, plot_plotly
@@ -12,69 +12,94 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from stock_object import Stock
 
-st.set_page_config(layout='wide',initial_sidebar_state='expanded')
-st.markdown('# Train forecasting models for stock prices')
-#------ layout setting---------------------------
-window_selection_c=st.sidebar.container()
-window_selection_c.markdown('## Watch list')
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+st.markdown("# Train forecasting models for stock prices")
+# ------ layout setting---------------------------
+window_selection_c = st.sidebar.container()
+window_selection_c.markdown("## Watch list")
 
-sub_columns=window_selection_c.columns(2)
-change_c=st.sidebar.container()
+sub_columns = window_selection_c.columns(2)
+change_c = st.sidebar.container()
 
-#----------Time window selection-----------------
-TODAY= Stock.nearest_business_day(datetime.date.today())
-DEFAULT_START=Stock.nearest_business_day(TODAY-datetime.timedelta(days=700))
-
-
-
-START=sub_columns[0].date_input('From',value=DEFAULT_START,max_value=TODAY-datetime.timedelta(days=1))
-START=Stock.nearest_business_day(START)
-END=sub_columns[1].date_input('To',value=TODAY,max_value=TODAY,min_value=START)
-END=Stock.nearest_business_day(END)
+# ----------Time window selection-----------------
+TODAY = Stock.nearest_business_day(datetime.date.today())
+DEFAULT_START = Stock.nearest_business_day(TODAY - datetime.timedelta(days=700))
 
 
-#---------------stock selection------------------
-STOCKS=np.array(['AAPL','GOOG','MSFT','GME','FB']) #TODO : include all stocks
-selected_stocks=window_selection_c.multiselect('',STOCKS,default='GOOG')
-
-chart_width=st.expander(label='chart width').slider('',1000,2800,1400)
-
-
-#------------------------Plot stock linecharts--------------------
+START = sub_columns[0].date_input(
+    "From", value=DEFAULT_START, max_value=TODAY - datetime.timedelta(days=1)
+)
+START = Stock.nearest_business_day(START)
+END = sub_columns[1].date_input("To", value=TODAY, max_value=TODAY, min_value=START)
+END = Stock.nearest_business_day(END)
 
 
-fig =go.Figure()
+# ---------------stock selection------------------
+STOCKS = np.array(["AAPL", "GOOG", "MSFT", "GME", "FB"])  # TODO : include all stocks
+selected_stocks = window_selection_c.multiselect("", STOCKS, default="GOOG")
+
+chart_width = st.expander(label="chart width").slider("", 1000, 2800, 1400)
+
+
+# ------------------------Plot stock linecharts--------------------
+
+
+fig = go.Figure()
 for choice in selected_stocks:
-    stock=Stock(symbol=choice)
-    stock.load_data(START,END,inplace=True)
-    fig=stock.plot_raw_data(fig)
+    stock = Stock(symbol=choice)
+    stock.load_data(START, END, inplace=True)
+    fig = stock.plot_raw_data(fig)
 
     with change_c:
         stock.show_delta()
 fig.update_layout(
-    margin=dict(l=0, r=0, t=0, b=0,pad=0),
+    margin=dict(l=0, r=0, t=0, b=0, pad=0),
     width=chart_width,
     autosize=False,
-    template='plotly_dark'
-    
+    template="plotly_dark",
 )
 
 
 st.write(fig)
 
+# ---------------------------------------------------------------------------------
+
+if "TEST_INTERVAL_LENGTH" not in st.session_state:
+    # set the initial default value of the slider widget
+    st.session_state.TEST_INTERVAL_LENGTH = 100
+
+if "TRAIN_INTERVAL_LENGTH" not in st.session_state:
+    # set the initial default value of the slider widget
+    st.session_state.TRAIN_INTERVAL_LENGTH = 200
+
+if "HORIZON" not in st.session_state:
+    # set the initial default value of the slider widget
+    st.session_state.HORIZON = 7
 
 
-
-#------------------------------------------Training configurations-------------------------------------
+# ------------------------------------------Training configurations-------------------------------------
 st.sidebar.markdown("## Forecasts")
-form=st.sidebar.form(key='train_dataset')
-SYMB=form.selectbox('select stock',STOCKS)
-form.markdown('## Select interval lengths')
-HORIZON=form.number_input('Inference horizon',value=7,min_value=7,max_value=200)
-TEST_INTERVAL_LENGTH=form.number_input('number of days to test on and visualize',value=30,min_value=20)
-TRAIN_INTERVAL_LENGTH=form.number_input('number of  day to use for training',value=300,min_value=100)
-form.form_submit_button(label='Load and train',on_click=Stock.train_forecast_report(chart_width,SYMB,TRAIN_INTERVAL_LENGTH,TEST_INTERVAL_LENGTH))
+form = st.sidebar.form(key="train_dataset")
+SYMB = form.selectbox("select stock", STOCKS)
+form.markdown("## Select interval lengths")
+HORIZON = form.number_input(
+    "Inference horizon", min_value=7, max_value=200, key="HORIZON"
+)
+TEST_INTERVAL_LENGTH = form.number_input(
+    "number of days to test on and visualize",
+    min_value=20,
+    key="TEST_INTERVAL_LENGTH",
+)
 
+TRAIN_INTERVAL_LENGTH = form.number_input(
+    "number of  day to use for training",
+    min_value=100,
+    key="TRAIN_INTERVAL_LENGTH",
+)
 
-
-
+form.form_submit_button(
+    label="Load and train",
+    on_click=Stock.train_forecast_report(
+        chart_width, SYMB, TRAIN_INTERVAL_LENGTH, TEST_INTERVAL_LENGTH
+    ),
+)
