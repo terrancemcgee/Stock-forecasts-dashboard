81113e2958740cbc0e9398404cbcec4cfac7a421
hamzarabi3 <googlinghamza@gmail.com>
2021-10-16 18:46:23 +0000

    forecaster is working

--- python_engineer_project.py
+++ python_engineer_project.py
@@ -1,5 +1,4 @@
 from mimetypes import init
-from os import terminal_size
 from numpy.core.fromnumeric import std
 import pandas as pd 
 import streamlit as st 
@@ -7,33 +6,38 @@ from prophet import Prophet, forecaster
 import datetime
 from prophet.plot import plot, plot_components, plot_plotly
 from plotly import graph_objects as go
-import plotly.express as px 
 from streamlit.type_util import data_frame_to_bytes
-import yfinance as yf
 import numpy as np
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from stock_object import Stock
 
 st.set_page_config(layout='wide',initial_sidebar_state='expanded')
-
+st.markdown('# Train forecasting models for stock prices')
 #------ layout setting---------------------------
 window_selection_c=st.sidebar.container()
-sub_columns=window_selection_c.columns(2)
+window_selection_c.markdown('## Watch list')
 
+sub_columns=window_selection_c.columns(2)
 change_c=st.sidebar.container()
 
 #----------Time window selection-----------------
-TODAY=datetime.date.today()
-START=sub_columns[0].date_input('From',value=TODAY-datetime.timedelta(days=700),max_value=TODAY-datetime.timedelta(days=1))
-END=sub_columns[1].date_input('To',value=TODAY,max_value=TODAY)
+TODAY= Stock.nearest_business_day(datetime.date.today())
+DEFAULT_START=Stock.nearest_business_day(TODAY-datetime.timedelta(days=700))
+
+
+
+START=sub_columns[0].date_input('From',value=DEFAULT_START,max_value=TODAY-datetime.timedelta(days=1))
+START=Stock.nearest_business_day(START)
+END=sub_columns[1].date_input('To',value=TODAY,max_value=TODAY,min_value=START)
+END=Stock.nearest_business_day(END)
 
 
 #---------------stock selection------------------
-STOCKS=np.array(['AAPL','GOOG','MSFT','GME']) #TODO : include all stocks
+STOCKS=np.array(['AAPL','GOOG','MSFT','GME','FB']) #TODO : include all stocks
+selected_stocks=window_selection_c.multiselect('',STOCKS,default='GOOG')
 
-selected_stocks=window_selection_c.multiselect('Select stocks ',STOCKS,default='GOOG')
-
+chart_width=st.expander(label='chart width').slider('',1000,2800,1400)
 
 
 #------------------------Plot stock linecharts--------------------
@@ -47,10 +51,9 @@ for choice in selected_stocks:
 
     with change_c:
         stock.show_delta()
-
 fig.update_layout(
     margin=dict(l=0, r=0, t=0, b=0,pad=0),
-    width=1300,
+    width=chart_width,
     autosize=False,
     template='plotly_dark'
     
@@ -58,5 +61,20 @@ fig.update_layout(
 
 
 st.write(fig)
+
+
+
+
+#------------------------------------------Training configurations-------------------------------------
+st.sidebar.markdown("## Forecasts")
+form=st.sidebar.form(key='train_dataset')
+SYMB=form.selectbox('select stock',STOCKS)
+form.markdown('## Select interval lengths')
+HORIZON=form.number_input('Inference horizon',value=7,min_value=7,max_value=200)
+TEST_INTERVAL_LENGTH=form.number_input('number of days to test on and visualize',value=30,min_value=20)
+TRAIN_INTERVAL_LENGTH=form.number_input('number of  day to use for training',value=300,min_value=100)
+form.form_submit_button(label='Load and train',on_click=Stock.train_forecast_report(chart_width,SYMB,TRAIN_INTERVAL_LENGTH,TEST_INTERVAL_LENGTH))
+
+
 
 
--- stock_object.py
+++ stock_object.py
@@ -1,8 +1,12 @@
+from prophet.forecaster import Prophet
 import yfinance as yf
 import datetime
 import plotly.express as px
 import streamlit as st
 import plotly.graph_objects as go
+import pandas as pd
+from typing import Optional, Any, Dict, Tuple
+from sklearn.metrics import mean_absolute_percentage_error
 class Stock:
     """
     This class enables data loading, plotting and statistical analysis of a given stock
@@ -31,6 +35,7 @@ class Stock:
         data.reset_index(inplace=True)
         data.rename(columns={'Date':'datetime'},inplace=True)
         data['date']=data.apply(lambda raw: raw['datetime'].date(),axis=1)
+        
         data=data[['date',self.column]]
         if inplace:
             self.data=data 
@@ -63,8 +68,119 @@ class Stock:
         cols=st.columns(2)
         (color,marker)=('green','+') if difference>=0 else ('red','-')
 
-        cols[0].markdown(f"""<p style="font-size: 100%;margin-left:10px">{self.symbol} {e}</p>""",unsafe_allow_html=True)
-        cols[1].markdown(f"""<p style="color:{color};font-size:100%;margin-right:10px">{marker} {difference} {marker} {change}</p>""",unsafe_allow_html=True)
+        cols[0].markdown(f"""<p style="font-size: 100%;margin-left:10px">{self.symbol} \t {e}</p>""",unsafe_allow_html=True)
+        cols[1].markdown(f"""<p style="color:{color};font-size:100%;margin-right:10px">{marker} \t {difference} {marker} {change}</p>""",unsafe_allow_html=True)
     
+    @staticmethod    
+    def nearest_business_day(DATE:datetime.date):
+        """
+        Takes a date and transform it to the nearest business day
+        """
+        if DATE.weekday() ==5:
+            DATE=DATE-datetime.timedelta(days=1)
+
+        if DATE.weekday()==6:
+            DATE=DATE+datetime.timedelta(days=1)
+        return DATE
+
 
     
+
+    @staticmethod
+    def for_prophet(df:pd.DataFrame,date_column='date',y_column='Close')-> pd.DataFrame:
+        return df.rename(columns={date_column:'ds',y_column:'y'})
+
+    @st.cache
+    def load_train_test_data(self,TEST_INTERVAL_LENGTH,TRAIN_INTERVAL_LENGTH):
+        """Returns two dataframes for testing and training"""
+        TODAY= Stock.nearest_business_day(datetime.date.today())
+        TEST_END=Stock.nearest_business_day(TODAY)
+        TEST_START=Stock.nearest_business_day(TEST_END-datetime.timedelta(days=TEST_INTERVAL_LENGTH))
+
+        TRAIN_END=Stock.nearest_business_day(TEST_START-datetime.timedelta(days=1))
+        TRAIN_START=Stock.nearest_business_day(TRAIN_END-datetime.timedelta(days=TRAIN_INTERVAL_LENGTH))
+
+        train_data=self.load_data(TRAIN_START,TRAIN_END)
+        test_data=self.load_data(TEST_START,TEST_END)
+
+        train_data=Stock.for_prophet(train_data)
+        test_data=Stock.for_prophet(test_data)
+        self.train_data=train_data 
+        self.test_data=test_data
+
+    @st.cache(suppress_st_warning=True)
+    def train_prophet(self,kwargs={}):
+        m=Prophet(**kwargs)
+        m.fit(self.train_data)
+        self.model=m
+        forecasts=m.predict(self.test_data)
+        self.test_data=self.test_data.join(forecasts[['yhat_lower','yhat','yhat_upper']])
+        self.test_mape=mean_absolute_percentage_error(self.test_data['y'],self.test_data['yhat'])
+
+    def plot_test(self,chart_width):
+        fig = go.Figure()
+        fig.add_trace(
+            go.Scatter(
+                x=self.test_data['ds'],
+                y=self.test_data['y'], 
+                mode='lines',
+                name='True Closing price'
+            )
+        )
+
+        fig.add_trace(
+            go.Scatter(
+                x=self.test_data['ds'], 
+                y=self.test_data['yhat'], 
+                mode='lines', 
+                name='Predicted CLosing price'
+            )
+        )
+
+        fig.add_trace(
+            go.Scatter(
+                x=self.test_data['ds'], 
+                y=self.test_data['yhat_upper'], 
+                fill=None,
+                mode='lines', 
+                name='CI+', 
+                line_color='orange'
+            )
+        )
+
+        fig.add_trace(
+            go.Scatter(
+                x=self.test_data['ds'], 
+                y=self.test_data['yhat_lower'], 
+                fill='tonexty',
+                mode='lines', 
+                line_color='orange',
+                name='CI-'
+            )
+        )
+        fig.update_layout(width=chart_width,
+                        margin=dict(l=0, r=0, t=0, b=0,pad=0),
+                        legend=dict(
+                                x=0,
+                                y=0.99,
+                                traceorder='normal',
+                                font=dict(
+                                    size=12),
+                            ),
+                        autosize=False,
+                        template='plotly_dark')
+        
+        return fig
+
+    @staticmethod 
+    def train_forecast_report(chart_width,symb,TRAIN_INTERVAL_LENGTH,TEST_INTERVAL_LENGTH):
+        stock=Stock(symb)
+        stock.load_train_test_data(TEST_INTERVAL_LENGTH,TRAIN_INTERVAL_LENGTH)
+        stock.train_prophet()
+        fig=stock.plot_test(chart_width)
+        st.markdown(f'## {symb} stock forecasts on testing set, Testing error {round(stock.test_mape*100,2)}%')
+        st.plotly_chart(fig)
+        
+
+
+
